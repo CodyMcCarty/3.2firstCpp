3c87742e68238895244ba0ea24bee15231fe2824
Cody McCarty <codyjmccarty@gmail.com>
2022-11-10 12:49:20 -0600

    errors and warnings

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-//This brings in the iostream library
 #include <iostream>
 
-/*
-this is a 
-block comment
-*/
-
-
 int main(){
-    std::cout << "Hello World" << std::endl;
-    
+    /*
+    //Compile time error
+    std::cout << "Hello World" << std::endl
+    */
+
+    /*
+    //Run time error
+    std::cout << "Start\n";
+    int value = 0;
+    std::cout << 7 / value << std::endl;
+    std::cout << "End\n"; // doesn't print this
+    */
+
     return 0;
 }
