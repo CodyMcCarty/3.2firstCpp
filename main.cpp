435104d04543634e6a4c7aa8e5a22c18f841a087
Cody McCarty <codyjmccarty@gmail.com>
2022-11-10 12:04:05 -0600

    cout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << "this " << result << std::endl;
+    std::cout << "Hello World" << std::endl;
+    
+    return 0;
 }
